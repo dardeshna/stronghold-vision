8a5ea3cb410663a070d8942adfbdcef4f4faa7ca
dardeshna <devin@ardeshna.com>
2016-10-13 19:17:38 -0700

    edited to work with axis cam and live stream

--- tyr-vision.py
+++ tyr-vision.py
@@ -6,27 +6,22 @@ import urllib2
 from networktables import NetworkTable
 import operator
 from math import *
-import targeting
 
-"""
 stream = urllib2.urlopen("http://10.0.8.3/mjpg/video.mjpg")
 IP = "roborio-8-frc.local"
 NetworkTable.setIPAddress(IP)
 NetworkTable.setClientMode()
 NetworkTable.initialize()
-"""
 
-WIDTH = 1280
-HEIGHT = 720
-f = 900
+WIDTH = 640
+HEIGHT = 480
+f = 282.2047
 h = 7
-
-cap = cv2.VideoCapture('assets/12ft.mp4')
 
 table = NetworkTable.getTable("visiondata")
 
-COLOR_MIN = np.array([230, 230, 230], np.uint8) #min and max hsv thresholds
-COLOR_MAX = np.array([255, 255, 255], np.uint8)
+COLOR_MIN = np.array([40, 75, 130], np.uint8) #min and max hsv thresholds
+COLOR_MAX = np.array([65, 254, 254], np.uint8)
 
 def draw_HUD(img, x, y, fps, angle, calculated):
     cv2.rectangle(img, (0, 0), (380, 70), (255, 255, 255), 2)
@@ -46,22 +41,16 @@ def main():
     times = [time.time()]
 
     while True:
-        """
+        
         bytes += stream.read(1024) #read the bytes
         a = bytes.find('\xff\xd8')
         b = bytes.find('\xff\xd9')
-        """
-        a, b = 0, 0
+        
         if a!=-1 and b!=-1:
             
-            """
             jpg = bytes[a:b+2]
             bytes= bytes[b+2:]
             img = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),1) #turn into image
-            """
-            
-            ret, img = cap.read()
-            if ret==False: break
             
             total_frames += 1 #calculations for fps
             times.append(time.time())
@@ -69,41 +58,48 @@ def main():
                 times.pop(0)
             fps = (int)(1.0 / (times[-1] - times[-2]))
 
-            #flipped_img = cv2.flip(img, 1)
-            flipped_img = img
+            flipped_img = cv2.resize(img, (640, 480))
+            hsv_img = cv2.cvtColor(flipped_img, cv2.COLOR_BGR2HSV) #convert image to HSV
+            threshold = cv2.inRange(hsv_img, COLOR_MIN, COLOR_MAX) #hsv threshold 
 
-            approx = targeting.find_best_match(flipped_img) 
-            
-            if approx != None: #put contours in an array
-                corners = getCorners(approx)
-         
+            areaArray = []
+            contours, heirarchy = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE) #find all contours in the image
+
+            if len(contours) != 0: #put contours in an array
+                for i, c in enumerate(contours):
+                        area = cv2.contourArea(c)
+                        areaArray.append(area)
+
+                sortedData = sorted(zip(areaArray, contours), key=lambda x: x[0], reverse = True) #sort array for biggest contour
+                largestContour = sortedData[0][1]
+
+                epsilon = 0.012 * cv2.arcLength(largestContour, True) 
+                approx = cv2.approxPolyDP(largestContour, epsilon, True) #approvimate polygon from contour
+
                 x = corners[0][0]
                 y = corners[0][1]
 
-                if corners != None:
-                    for i in corners:
-                        cv2.circle(img, (i[0], i[1]), 3, (0, 255, 0)) #line from screen center to goal edge
-
-
+                        
                 calculated = calculate(corners)
-                
                 angle = np.rad2deg(np.arctan((x - WIDTH/2) / f))
-                draw_HUD(flipped_img, x, y, fps, angle, calculated) #draw the hud on the flipped image
-                cv2.drawContours(flipped_img, [approx], -1, (255, 150, 0), 2) #draw the contours on the flipped image
 
                 table.putNumber("skewangle", angle)
                 table.putNumber("xdisplacement", (WIDTH/2 - x))
-
+                
+                draw_HUD(flipped_img, x, y, fps, angle, calculated) #draw the hud on the flipped image
+                cv2.drawContours(flipped_img, [approx], -1, (255, 150, 0), 2) #draw the contours on the flipped image
+                if corners != None:
+                    for i in corners:
+                        cv2.circle(img, (i[0], i[1]), 3, (0, 255, 0)) #green circles at each corner
                 cv2.imshow('tyr-vision', flipped_img) #create a window with the complete image
-            else :
-                angle = 0
+                
+            else:
                 calculated = (0, 0, 0, 0)
-                #print 'Nothing found. '
+                angle = 0
                 table.putNumber("skewangle", 100000) #values at 100,000 if nothing found
                 table.putNumber("xdisplacement", 100000)
-                draw_HUD(flipped_img, 319, 239, fps, angle, calculated) #draw a hud with no contour detected 
+                draw_HUD(flipped_img, 320, 240, fps, angle, calculated) #draw a hud with no contour detected 
                 cv2.imshow('tyr-vision', flipped_img)
-
 
             key = cv2.waitKey(30) & 0xff #press ESC to quit
             if key == 27:
@@ -186,44 +182,6 @@ def calculate(corners):
     distance = sqrt(best_l**2+best_d**2)
     skew = degrees(atan(best_l/best_d))
 
-    return (best_l, best_d, distance, skew)
-
-
-# Not used
-def optimize(alpha):
-    
-    best_l, best_d, max_error = 0, 0, 0
-    
-    #x: -14, 14
-    #y: 0, 18
-    #terribly inefficient
-    for l in range(-28, 29):
-        l = l/2.0
-        for d in range(0, 37):
-
-            d = d/2.0
-
-            vectors_p = []
-            alpha_p = []
-
-            vectors_p.append(normalize(l, h, d))
-            vectors_p.append(normalize(l+1.5, h, d))
-            vectors_p.append(normalize(l+1.5, h-1.16666667, d))
-            vectors_p.append(normalize(l, h-1.16666667, d))
-
-            for i in range(0, len(vectors_p)):
-                for j in range(i+1, len(vectors_p)):
-                    alpha_p.append(degrees(acos(max(-1.0, min(1.0, np.dot(vectors_p[i], vectors_p[j]))))))
-
-            error = 1/getError(alpha, alpha_p)
-            if error > max_error:
-                best_l = l
-                best_d = d
-                max_error = error
-
-    distance = sqrt(best_l**2+best_d**2)
-    skew = degrees(atan(best_l/best_d))
-    
     return (best_l, best_d, distance, skew)
 
     
