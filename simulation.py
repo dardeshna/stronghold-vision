3d6fbcde08e9aba045cb258dce94917a11ef5c6f
Devin Ardeshna <devin@ardeshna.com>
2017-08-15 18:31:34 -0700

    latest updates

---
+++ simulation.py
@@ -0,0 +1,138 @@
+import cv2
+import numpy as np
+import scipy.optimize
+import time
+import operator
+from math import *
+import random
+
+WIDTH = 640
+HEIGHT = 480
+
+f = 566.0699 #calculated for iPhone SE
+#f = 252 #old value for iPhone SE
+
+goal_width = 1.6875
+goal_height = 0.9583
+
+goal_points = np.float32([[-goal_width/2, -goal_height/2, 0],
+                          [goal_width/2, -goal_height/2, 0],
+                          [goal_width/2, goal_height/2, 0],
+                          [-goal_width/2, goal_height/2, 0]])
+goal_behind_points = np.float32([[-goal_width/2, -goal_height/2, .5],
+                          [goal_width/2, -goal_height/2, .5],
+                          [goal_width/2, goal_height/2, .5],
+                          [-goal_width/2, goal_height/2, .5]])
+
+K = np.float32([[f, 0, 0.5*(WIDTH-1)],
+                        [0, f, 0.5*(HEIGHT-1)],
+                        [0.0,0.0, 1.0]])
+
+global counter
+
+output = open('output.csv', 'w')
+
+"""
+#Green HSV Values
+COLOR_MIN = np.array([40, 75, 130], np.uint8) #min and max hsv thresholds
+COLOR_MAX = np.array([65, 254, 254], np.uint8)
+"""
+
+COLOR_MIN = np.array([225, 225, 225], np.uint8) #min and max hsv thresholds
+COLOR_MAX = np.array([255, 255, 255], np.uint8)
+
+def draw_HUD(img, fps, x, y, z, distance, skew, angle):
+    cv2.rectangle(img, (0, 0), (380, 70), (255, 255, 255), 2)
+    cv2.rectangle(img, (WIDTH-60, 0), (WIDTH-2, 40), (255, 255, 255), 2)
+    text = "  D: %.1f    A: %.1fdeg    S: %.1fdeg" % (distance, skew, angle)
+    text2 = "  X: %.1f    Y: %.1f    Z: %.1f" % (x, y, z)
+    cv2.putText(img, "%s" % text, (2, 28), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0)) #x and y displacement
+    cv2.putText(img, "%s" % text2, (2, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0)) #x and y displacement
+    cv2.putText(img, "FPS: %s" % fps, (582, 26), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0)) #FPS meter
+
+def main():
+    initial_time = time.time()
+    total_frames = 0
+    times = [time.time()]
+
+    counter = 0
+
+    while True:
+        
+        img = np.zeros((HEIGHT,WIDTH,3), np.uint8)
+        img[:,:] = (32,32,32)
+        
+        total_frames += 1 #calculations for fps
+        times.append(time.time())
+        if len(times) > 10:
+            times.pop(0)
+        fps = (int)(1.0 / (times[-1] - times[-2]))
+
+        pos = np.float32([sin(counter/25.0+12.5), 0, -6+2*cos(counter/50.0)])
+        rot = np.multiply(0*0.784/4*-cos(counter/25.0 + 10), np.array(normalize(.2,1,.5)))
+        
+        a_skew = degrees(atan(pos[0]/pos[2]))
+        
+        rvec = np.transpose(cv2.Rodrigues(rot)[0])
+        tvec = np.float32(np.multiply(-1, np.dot(rvec, pos)))
+        
+        goal = cv2.projectPoints(goal_points, rvec, tvec, K, None)
+        goal_behind = cv2.projectPoints(goal_behind_points, rvec, tvec, K, None)
+
+        for i in goal_behind[0]:
+            cv2.circle(img, (i[0][0], i[0][1]), 3, (0, 0, 255)) #red circles at each corner
+            
+        for i in goal[0]:
+            cv2.circle(img, (i[0][0], i[0][1]), 3, (0, 255, 0)) #green circles at each corner
+        """
+        for i in goal[0]:
+            i[0][0] += random.randint(-1, 1)
+            i[0][1] += random.randint(-1, 1)
+        """
+        
+        corners = goal[0]
+
+        ret, c_rvec, c_tvec = cv2.solvePnP(goal_points, np.float32(corners), K, None)
+
+        c_pos = np.multiply(-1, np.dot(np.linalg.inv(cv2.Rodrigues(c_rvec)[0]), c_tvec))
+
+        c_rot = cv2.Rodrigues(np.transpose(cv2.Rodrigues(c_rvec)[0]))[0]
+        
+        x = c_pos[0]
+        y = c_pos[1]
+        z = c_pos[2]
+        distance = sqrt(c_pos[0]**2+c_pos[2]**2)
+        skew = degrees(atan(c_pos[0]/c_pos[2]))
+        angle = degrees(c_rot[1]) - skew
+
+        v_obj = normalize(c_pos[0]+goal_width/2, c_pos[1]+goal_height/2, c_pos[2])
+        v_img = normalize(0.5*(WIDTH-1)-corners[0][0][0], 0.5*(HEIGHT-1)-corners[0][0][1], -f)
+
+        cross = np.cross(v_obj, v_img)
+        angle2 = acos(max(min(np.dot(v_obj, v_img), 1.0), -1))
+        
+        angle2 = cross[1]*degrees(angle2) - skew
+        
+        #output.write(str(float((c_pos[0]-pos[0]))) + ", " + str(float((c_pos[1]-pos[1]))) + ", " + str(float((c_pos[2]-pos[2]))) + ", " + str(float(sqrt(c_pos[0]**2+c_pos[2]**2)-sqrt(pos[0]**2+pos[2]**2))) + ", " + str(float(sqrt(c_pos[0]**2+c_pos[2]**2))) + "\n") 
+        output.write(str(float(degrees(rot[1]))) + ", " + str(float(degrees(rot[1]) - a_skew)) + ", " + str(float(degrees(c_rot[1]) - skew)) + ", " + str(float((angle2))) + "\n")
+        
+        draw_HUD(img, fps, x, y, z, distance, skew, angle) #draw the hud on the flipped image
+        cv2.imshow('tyr-vision', img) #create a window with the complete image
+
+        counter = counter + 1
+        
+        key = cv2.waitKey(30) & 0xff #press ESC to quit
+        if key == 27:
+            print "Total frames: %d" %total_frames
+            break
+        
+    cv2.destroyAllWindows()
+    output.close()
+    
+def normalize(x, y, z):
+    x, y, z = float(x), float(y), float(z)
+    d = sqrt(x**2+y**2+z**2)
+    return (x/d, y/d, z/d)
+
+if __name__ == '__main__':
+        main()
--- tyr-vision2.py
+++ tyr-vision2.py
@@ -6,7 +6,7 @@ from networktables import NetworkTable
 import operator
 from math import *
 
-cap = cv2.VideoCapture("http://10.32.100.210/live")
+cap = cv2.VideoCapture("http://10.0.8.23/live")
 
 WIDTH = 640
 HEIGHT = 480
@@ -17,29 +17,27 @@ f = 566.0699 #calculated for iPhone SE
 goal_width = 1.6875
 goal_height = 0.9583
 
-goal_points = np.float32([[0, 0, 0],
-                          [goal_width, 0, 0],
-                          [goal_width, goal_height, 0],
-                          [0, goal_height, 0]])
+goal_points = np.float32([[-goal_width/2, -goal_height/2, 0],
+                          [goal_width/2, -goal_height/2, 0],
+                          [goal_width/2, goal_height/2, 0],
+                          [-goal_width/2, goal_height/2, 0]])
+goal_behind_points = np.float32([[-goal_width/2, -goal_height/2, .5],
+                          [goal_width/2, -goal_height/2, .5],
+                          [goal_width/2, goal_height/2, .5],
+                          [-goal_width/2, goal_height/2, .5]])
 
-"""
-#Green HSV Values
-COLOR_MIN = np.array([40, 75, 130], np.uint8) #min and max hsv thresholds
-COLOR_MAX = np.array([65, 254, 254], np.uint8)
-"""
-
-threshold = 0
+K = np.float32([[f, 0, 0.5*(WIDTH-1)],
+                        [0, f, 0.5*(HEIGHT-1)],
+                        [0.0,0.0, 1.0]])
 
 COLOR_MIN = np.array([225, 225, 225], np.uint8) #min and max hsv thresholds
 COLOR_MAX = np.array([255, 255, 255], np.uint8)
 
-def draw_HUD(img, x, y, fps, angle, calculated):
+def draw_HUD(img, fps, x, y, z, distance, skew, angle):
     cv2.rectangle(img, (0, 0), (380, 70), (255, 255, 255), 2)
     cv2.rectangle(img, (WIDTH-60, 0), (WIDTH-2, 40), (255, 255, 255), 2)
-    displacement_x = x-WIDTH/2
-    displacement_y = -y+HEIGHT/2
-    text = "<%d, %d> A: %d    D: %.1f    S: %.1fdeg" % (displacement_x, displacement_y, np.around(angle, 1), calculated[3], calculated[4])
-    text2 = "           X: %.1f    Y: %.1f    Z: %.1f" % (calculated[0], calculated[1], calculated[2])
+    text = "  D: %.1f    A: %.1fdeg    S: %.1fdeg" % (distance, angle, skew)
+    text2 = "  X: %.1f    Y: %.1f    Z: %.1f" % (x, y, z)
     cv2.putText(img, "%s" % text, (2, 28), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0)) #x and y displacement
     cv2.putText(img, "%s" % text2, (2, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0)) #x and y displacement
     cv2.putText(img, "FPS: %s" % fps, (582, 26), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0)) #FPS meter
@@ -81,34 +79,30 @@ def main():
             approx = cv2.approxPolyDP(largestContour, epsilon, True) #approvimate polygon from contour
             
             corners = getCorners(approx)
-
-            x = corners[0][0]
-            y = corners[0][1]
-
-            K = np.float32([[f, 0, 0.5*(WIDTH-1)],
-                        [0, f, 0.5*(HEIGHT-1)],
-                        [0.0,0.0, 1.0]])
             
-            ret, rvec, tvec = cv2.solvePnP(goal_points, np.float32((corners, )), K, None, flags=2)
+            ret, c_rvec, c_tvec = cv2.solvePnP(goal_points, np.float32(corners), K, None)
 
-            calculated = (tvec[0], tvec[2], tvec[1], 0, 0)
-                                           
-            angle = np.rad2deg(np.arctan((x - WIDTH/2) / f))
+            c_pos = np.multiply(-1, np.dot(np.linalg.inv(cv2.Rodrigues(c_rvec)[0]), c_tvec))
 
-            draw_HUD(img, x, y, fps, angle, calculated) #draw the hud on the flipped image
-            cv2.drawContours(img, [approx], -1, (255, 150, 0), 2) #draw the contours on the flipped image
-        
-            if corners != None:
-                for i in corners:
-                    cv2.circle(img, (i[0], i[1]), 3, (0, 255, 0)) #green circles at each corner
+            c_rot = cv2.Rodrigues(np.transpose(cv2.Rodrigues(c_rvec)[0]))[0]
+            
+            x = c_pos[0]
+            y = c_pos[1]
+            z = c_pos[2]
+            distance = sqrt(c_pos[0]**2+c_pos[2]**2)
+            skew = degrees(atan(c_pos[0]/c_pos[2]))
+            angle = degrees(c_rot[1]) - skew
+            
+            draw_HUD(img, fps, x, y, z, distance, skew, angle) #draw the hud on the flipped image
+            
+            cv2.drawContours(img, [corners], -1, (255, 150, 0), 2) #draw the contours on the flipped image
+            for i in corners:
+                cv2.circle(img, (i[0][0], i[0][1]), 3, (0, 255, 0)) #green circles at each corner
+                
             cv2.imshow('tyr-vision', img) #create a window with the complete image
-            
+
         else:
-            
-            calculated = (0, 0, 0, 0, 0)
-            angle = 0
-            
-            draw_HUD(img, 320, 240, fps, angle, calculated) #draw a hud with no contour detected 
+            draw_HUD(img, fps, 0, 0, 0, 0, 0, 0) #draw the hud on the flipped image
             cv2.imshow('tyr-vision', img)
 
         key = cv2.waitKey(30) & 0xff #press ESC to quit
@@ -117,6 +111,7 @@ def main():
             break
         
     cv2.destroyAllWindows()
+    exit()
 
 # returns (top left, top right, bottom right, bottom left)
 def getCorners(approx):
@@ -132,68 +127,9 @@ def getCorners(approx):
 
     points = []
     for i in range(0,4):
-        points.append(tuple(approx[min(enumerate([row[i] for row in distances]), key=operator.itemgetter(1))[0]][0]))
+        points.append(np.array([approx[min(enumerate([row[i] for row in distances]), key=operator.itemgetter(1))[0]][0]]))
 
-    return tuple(points)
-
-def normalize(x, y, z):
-    x, y, z = float(x), float(y), float(z)
-    d = sqrt(x**2+y**2+z**2)
-    return (x/d, y/d, z/d)
-
-
-def getError(a, b):
-    if len(a) != len(b): return 0
-
-    error = 0
-    for i in range(0, len(a)):
-        error+=(a[i]-b[i])**2
-
-    return error
-
-def E(x, alpha):
-
-    l = x[0]
-    d = x[1]
-    h = x[2]
-
-    vectors_p = []
-    alpha_p = []
-
-    vectors_p.append(normalize(l, h, d))
-    vectors_p.append(normalize(l+goal_width, h, d))
-    vectors_p.append(normalize(l+goal_width, h-goal_height, d))
-    vectors_p.append(normalize(l, h-goal_height, d))
-
-    for i in range(0, len(vectors_p)):
-        for j in range(i+1, len(vectors_p)):
-            alpha_p.append(degrees(acos(max(-1.0, min(1.0, np.dot(vectors_p[i], vectors_p[j]))))))
-
-    return getError(alpha, alpha_p)    
-
-
-def calculate(corners):
-
-    alpha = []
-    vectors = []
-    
-    for i in corners:
-        u = i[0]-WIDTH/2
-        v = -(i[1]-HEIGHT/2)
-        vectors.append(normalize(u, v, f))
-    
-    for i in range(0, len(vectors)):
-        for j in range(i+1, len(vectors)):
-            alpha.append(degrees(acos(max(-1.0, min(1.0, np.dot(vectors[i], vectors[j]))))))
-
-    minimize = scipy.optimize.minimize(E, (0, 8, 0), args=(alpha, ), method='SLSQP')
-
-    best_l, best_d, hi = minimize['x']
-
-    distance = sqrt(best_l**2+best_d**2)
-    skew = degrees(atan(best_l/best_d))
-
-    return (best_l, best_d, hi, distance, skew)
+    return np.array(points)
 
 
 if __name__ == '__main__':
